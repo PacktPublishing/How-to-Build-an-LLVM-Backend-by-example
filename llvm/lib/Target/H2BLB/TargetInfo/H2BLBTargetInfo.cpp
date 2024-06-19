0992290c4f5cb5e1d2c4857016b49034b7eb550a
Quentin Colombet <quentin.colombet@gmail.com>
2024-06-19 21:30:57 +0200

    [H2BLB] Placeholder for the upcoming backend
    Add the bare minimum to be able to build the H2BLB.
    
    This means featuring:
    - a LLVMInitializeH2BLBTargetMC that is being called from
      InitializeAllTargetMCs
    - a LLVMInitializeH2BLBTargetInfo that is being called from
      InitializeAllTargetInfos
    - a LLVMInitializeH2BLBTarget that is being called from
      InitializeAllTargets
    
    The actual structure of the directories set us on the right foot
    for what is coming.
    
    Thanks to this commit, we can use H2BLB in -DLLVM_TARGETS_TO_BUILD.
    That being said, it doesn't do anything useful.

---
+++ llvm/lib/Target/H2BLB/TargetInfo/H2BLBTargetInfo.cpp
@@ -0,0 +1,11 @@
+//===-- H2BLBTargetInfo.cpp - H2BLB Target Implementation -----------------===//
+//
+// Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+//
+//===----------------------------------------------------------------------===//
+
+#include "llvm/Support/Compiler.h" // For LLVM_EXTERNAL_VISIBILITY.
+
+extern "C" LLVM_EXTERNAL_VISIBILITY void LLVMInitializeH2BLBTargetInfo() {}
---
+++ llvm/lib/Target/H2BLB/TargetInfo/H2BLBTargetInfo.h
@@ -0,0 +1,12 @@
+//===-- H2BLBTargetInfo.h - H2BLB Target Implementation -----*- C++ -*-===//
+//
+// Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+//
+//===----------------------------------------------------------------------===//
+
+#ifndef LLVM_LIB_TARGET_H2BLB_TARGETINFO_H2BLBTARGETINFO_H
+#define LLVM_LIB_TARGET_H2BLB_TARGETINFO_H2BLBTARGETINFO_H
+
+#endif // LLVM_LIB_TARGET_H2BLB_TARGETINFO_H2BLBTARGETINFO_H

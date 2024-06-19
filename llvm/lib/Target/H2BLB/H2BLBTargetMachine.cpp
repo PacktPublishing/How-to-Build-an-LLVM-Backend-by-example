23fe4fc75fefab8e4fc4676015552037afaedfb3
Quentin Colombet <quentin.colombet@gmail.com>
2024-06-19 21:30:57 +0200

    [H2BLB] Placeholder for the upcoming backend
    Add the bare minimum to be able to build the H2BLB.
    
    This means featuring:
    - a LLVMInitializeH2BLBTargetMC that is being called from
      InitializeAllTargetMCs
    - a LLVMInitializeH2BLBTargetInfo that is being called from
      InitializeAllTargetInfos
    - a LLVMInitializeH2BLBTarget that is being called from
      InitializeAllTargets
    
    The actual structure of the directories set us on the right foot
    for what is coming.
    
    Thanks to this commit, we can use H2BLB in -DLLVM_TARGETS_TO_BUILD.
    That being said, it doesn't do anything useful.

---
+++ llvm/lib/Target/H2BLB/H2BLBTargetMachine.cpp
@@ -0,0 +1,15 @@
+//===-- H2BLBTargetMachine.cpp - Define TargetMachine for H2BLB -----------===//
+//
+// Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+//
+//===----------------------------------------------------------------------===//
+//
+// Implements the info about H2BLB target spec.
+//
+//===----------------------------------------------------------------------===//
+
+#include "llvm/Support/Compiler.h" // For LLVM_EXTERNAL_VISIBILITY.
+
+extern "C" LLVM_EXTERNAL_VISIBILITY void LLVMInitializeH2BLBTarget() {}

d5c7f3e21abf951a5ee94ec6379645cf17a1bee0
Quentin Colombet <quentin.colombet@gmail.com>
2024-06-29 22:30:00 +0200

    [clang][H2BLB] Expose some builtins
    This commit plumbs through Clang the widening multiply intrinsics added
    in the previous commit.
    
    This commit uses the old style way of defining Clang builtins.

--- clang/lib/Basic/Targets/H2BLB.cpp
+++ clang/lib/Basic/Targets/H2BLB.cpp
@@ -12,6 +12,7 @@
 
 #include "Targets/H2BLB.h"
 #include "clang/Basic/MacroBuilder.h"
+#include "clang/Basic/TargetBuiltins.h"
 
 using namespace clang;
 using namespace clang::targets;
@@ -19,4 +20,19 @@ using namespace clang::targets;
 void H2BLBTargetInfo::getTargetDefines(const LangOptions &Opts,
                                        MacroBuilder &Builder) const {
   Builder.defineMacro("__H2BLB__", "1");
+}
+
+static constexpr Builtin::Info BuiltinInfo[] = {
+#define BUILTIN(ID, TYPE, ATTRS)                                               \
+  {#ID, TYPE, ATTRS, nullptr, HeaderDesc::NO_HEADER, ALL_LANGUAGES},
+#define LIBBUILTIN(ID, TYPE, ATTRS, HEADER)                                    \
+  {#ID, TYPE, ATTRS, nullptr, HEADER, ALL_LANGUAGES},
+#define TARGET_BUILTIN(ID, TYPE, ATTRS, FEATURE)                               \
+  {#ID, TYPE, ATTRS, FEATURE, HeaderDesc::NO_HEADER, ALL_LANGUAGES},
+#include "clang/Basic/BuiltinsH2BLB.def"
+};
+
+ArrayRef<Builtin::Info> H2BLBTargetInfo::getTargetBuiltins() const {
+  return llvm::ArrayRef(BuiltinInfo,
+                        clang::H2BLB::LastTSBuiltin - Builtin::FirstTSBuiltin);
 }

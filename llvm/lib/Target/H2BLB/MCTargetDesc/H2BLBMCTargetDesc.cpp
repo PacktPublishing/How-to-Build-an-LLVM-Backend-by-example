5762a7b1a02f06240328c60180f3453e5d64ad36
Quentin Colombet <quentin.colombet@gmail.com>
2024-07-04 20:43:54 +0200

    [H2BLB] Plugged in the subtarget info
    Right now the subtarget info is just an empty shell, but it is
    required to enable the use of the legacy pass manager.
    The legacy pass manager is connected to the codegen path and codegen
    needs this kind of low level details.

--- llvm/lib/Target/H2BLB/MCTargetDesc/H2BLBMCTargetDesc.cpp
+++ llvm/lib/Target/H2BLB/MCTargetDesc/H2BLBMCTargetDesc.cpp
@@ -10,6 +10,26 @@
 //
 //===----------------------------------------------------------------------===//
 
-#include "llvm/Support/Compiler.h" // For LLVM_EXTERNAL_VISIBILITY.
+#include "H2BLBMCTargetDesc.h"
+#include "TargetInfo/H2BLBTargetInfo.h" // For getTheH2BLBTarget.
+#include "llvm/MC/MCSubtargetInfo.h"
+#include "llvm/Support/Compiler.h"  // For LLVM_EXTERNAL_VISIBILITY.
+#include "llvm/TargetParser/Triple.h"
 
-extern "C" LLVM_EXTERNAL_VISIBILITY void LLVMInitializeH2BLBTargetMC() {}
+using namespace llvm;
+
+#define GET_SUBTARGETINFO_MC_DESC
+#include "H2BLBGenSubtargetInfo.inc"
+
+static MCSubtargetInfo *
+createH2BLBMCSubtargetInfo(const Triple &TT, StringRef CPU, StringRef FS) {
+  return createH2BLBMCSubtargetInfoImpl(TT, CPU, /*TuneCPU*/ CPU, FS);
+}
+
+extern "C" LLVM_EXTERNAL_VISIBILITY void LLVMInitializeH2BLBTargetMC() {
+  Target &TheTarget = getTheH2BLBTarget();
+
+  // Register the MC subtarget info.
+  TargetRegistry::RegisterMCSubtargetInfo(TheTarget,
+                                          createH2BLBMCSubtargetInfo);
+}
---
+++ llvm/lib/Target/H2BLB/MCTargetDesc/H2BLBMCTargetDesc.h
@@ -0,0 +1,20 @@
+//===-- H2BLBMCTargetDesc.h - H2BLB Target Descriptions ---------*- C++ -*-===//
+//
+// Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+//
+//===----------------------------------------------------------------------===//
+//
+/// Provides H2BLB specific target descriptions.
+//
+//===----------------------------------------------------------------------===//
+//
+
+#ifndef LLVM_LIB_TARGET_H2BLB_MCTARGETDESC_H2BLBMCTARGETDESC_H
+#define LLVM_LIB_TARGET_H2BLB_MCTARGETDESC_H2BLBMCTARGETDESC_H
+
+#define GET_SUBTARGETINFO_ENUM
+#include "H2BLBGenSubtargetInfo.inc"
+
+#endif

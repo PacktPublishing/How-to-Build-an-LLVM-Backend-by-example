b559492d36e60fc9895126b4aaa217554e4f68ad
Quentin Colombet <quentin.colombet@gmail.com>
2024-07-04 20:52:46 +0200

    [H2BLB] Plugged the MCRegisterInfo
    This is just an empty shell, but it is required to plug the codegen
    pipeline.

--- llvm/lib/Target/H2BLB/MCTargetDesc/H2BLBMCTargetDesc.cpp
+++ llvm/lib/Target/H2BLB/MCTargetDesc/H2BLBMCTargetDesc.cpp
@@ -12,6 +12,7 @@
 
 #include "H2BLBMCTargetDesc.h"
 #include "TargetInfo/H2BLBTargetInfo.h" // For getTheH2BLBTarget.
+#include "llvm/MC/MCRegisterInfo.h"
 #include "llvm/MC/MCSubtargetInfo.h"
 #include "llvm/Support/Compiler.h"  // For LLVM_EXTERNAL_VISIBILITY.
 #include "llvm/TargetParser/Triple.h"
@@ -21,6 +22,12 @@ using namespace llvm;
 #define GET_SUBTARGETINFO_MC_DESC
 #include "H2BLBGenSubtargetInfo.inc"
 
+static MCRegisterInfo *createH2BLBMCRegisterInfo(const Triple &Triple) {
+  MCRegisterInfo *X = new MCRegisterInfo();
+  // TODO: Fill out the register info.
+  return X;
+}
+
 static MCSubtargetInfo *
 createH2BLBMCSubtargetInfo(const Triple &TT, StringRef CPU, StringRef FS) {
   return createH2BLBMCSubtargetInfoImpl(TT, CPU, /*TuneCPU*/ CPU, FS);
@@ -28,6 +35,9 @@ createH2BLBMCSubtargetInfo(const Triple &TT, StringRef CPU, StringRef FS) {
 
 extern "C" LLVM_EXTERNAL_VISIBILITY void LLVMInitializeH2BLBTargetMC() {
   Target &TheTarget = getTheH2BLBTarget();
+
+  // Register the MC register info.
+  TargetRegistry::RegisterMCRegInfo(TheTarget, createH2BLBMCRegisterInfo);
 
   // Register the MC subtarget info.
   TargetRegistry::RegisterMCSubtargetInfo(TheTarget,

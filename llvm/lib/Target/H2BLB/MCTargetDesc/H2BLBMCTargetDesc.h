de8bd141695679241992606836f90ef57be0745c
Quentin Colombet <quentin.colombet@gmail.com>
2024-09-08 16:11:18 +0200

    [H2BLB] Hook up the MCRegisterInfo generated by TableGen

--- llvm/lib/Target/H2BLB/MCTargetDesc/H2BLBMCTargetDesc.h
+++ llvm/lib/Target/H2BLB/MCTargetDesc/H2BLBMCTargetDesc.h
@@ -14,6 +14,13 @@
 #ifndef LLVM_LIB_TARGET_H2BLB_MCTARGETDESC_H2BLBMCTARGETDESC_H
 #define LLVM_LIB_TARGET_H2BLB_MCTARGETDESC_H2BLBMCTARGETDESC_H
 
+#include <cstdint> // For int16_t and so on used in the .inc files.
+
+// Defines symbolic names for H2BLB registers.  This defines a mapping from
+// register name to register number.
+#define GET_REGINFO_ENUM
+#include "H2BLBGenRegisterInfo.inc"
+
 #define GET_SUBTARGETINFO_ENUM
 #include "H2BLBGenSubtargetInfo.inc"
 

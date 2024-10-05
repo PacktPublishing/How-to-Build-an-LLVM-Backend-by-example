13af93b7eb891634826ca5b518fc5d493177a6be
Quentin Colombet <quentin.colombet@gmail.com>
2024-10-05 19:32:42 +0200

    [H2BLB][CallConv] Teach ISel about returns lowering
    The lowering of returns both for SDISel and FastISel goes through
    the TargetLowering::LowerReturn method.
    
    This patch implements this method and introduces a custom SDNode:
    `H2BLBreturnglue` in the process.
    This is required to tell the SDISel builder how to model our return
    instruction in the selection DAG intermediate representation.
    
    Additionally call to `computeRegisterProperties` and the code leading
    to it is mandatory to initialize the generic code with the types
    that are natively supported by the target and return the number of
    registers required for each type.

--- llvm/lib/Target/H2BLB/H2BLBISelLowering.cpp
+++ llvm/lib/Target/H2BLB/H2BLBISelLowering.cpp
@@ -20,8 +20,16 @@ using namespace llvm;
 
 #define DEBUG_TYPE "h2blb-lowering"
 
-H2BLBTargetLowering::H2BLBTargetLowering(const TargetMachine &TM)
-    : TargetLowering(TM) {}
+H2BLBTargetLowering::H2BLBTargetLowering(const TargetMachine &TM,
+                                         const H2BLBSubtarget &STI)
+    : TargetLowering(TM), Subtarget(STI) {
+  addRegisterClass(MVT::i16, &H2BLB::GPR16RegClass);
+  addRegisterClass(MVT::i32, &H2BLB::GPR32RegClass);
+
+  // Tell the generic implementation that we are done with setting up our
+  // register classes.
+  computeRegisterProperties(Subtarget.getRegisterInfo());
+}
 
 // Calling Convention Implementation
 #include "H2BLBGenCallingConv.inc"
@@ -30,4 +38,57 @@ FastISel *
 H2BLBTargetLowering::createFastISel(FunctionLoweringInfo &funcInfo,
                                     const TargetLibraryInfo *libInfo) const {
   return H2BLB::createFastISel(funcInfo, libInfo);
+}
+
+SDValue
+H2BLBTargetLowering::LowerReturn(SDValue Chain, CallingConv::ID CallConv,
+                                 bool IsVarArg,
+                                 const SmallVectorImpl<ISD::OutputArg> &Outs,
+                                 const SmallVectorImpl<SDValue> &OutVals,
+                                 const SDLoc &DL, SelectionDAG &DAG) const {
+
+  SmallVector<CCValAssign> RetValLocs;
+  MachineFunction &MF = DAG.getMachineFunction();
+
+  CCState CCInfo(CallConv, IsVarArg, MF, RetValLocs, *DAG.getContext());
+
+  CCInfo.AnalyzeReturn(Outs, RetCC_H2BLB_Common);
+
+  SDValue Glue;
+  SmallVector<SDValue> RetOps(1, Chain);
+
+  // Copy the result values into the output registers.
+  for (size_t i = 0, e = RetValLocs.size(); i != e; ++i) {
+    CCValAssign &VA = RetValLocs[i];
+    assert(VA.isRegLoc() && "stack return not yet implemented");
+    assert(VA.getLocInfo() == CCValAssign::Full &&
+           "extension/truncation of any sort, not yet implemented");
+
+    Chain = DAG.getCopyToReg(Chain, DL, VA.getLocReg(), OutVals[i], Glue);
+
+    // Guarantee that all emitted copies are stuck together,
+    // avoiding something bad.
+    Glue = Chain.getValue(1);
+    RetOps.push_back(DAG.getRegister(VA.getLocReg(), VA.getLocVT()));
+  }
+
+  // The return must have the last value of the chain.
+  // Update it now.
+  RetOps[0] = Chain;
+
+  // Add the glue if we have it.
+  if (Glue.getNode())
+    RetOps.push_back(Glue);
+
+  return DAG.getNode(H2BLBISD::RETURN_GLUE, DL, MVT::Other, RetOps);
+}
+
+const char *H2BLBTargetLowering::getTargetNodeName(unsigned Opcode) const {
+  switch ((H2BLBISD::NodeType)Opcode) {
+  case H2BLBISD::FIRST_NUMBER:
+    break;
+  case H2BLBISD::RETURN_GLUE:
+    return "H2BLBISD::RETURN_GLUE";
+  }
+  return nullptr;
 }

8b5d66f6c44abdbb37850931752b4e634bec52e0
Quentin Colombet <quentin.colombet@gmail.com>
2024-10-06 11:44:24 +0200

    [H2BLB] Buff up the calling convention support
    Move the calling convention information in its own file to be
    able to share the logic between SDISel and FastISel.
    
    Add a calling convention (called common) for lowering arguments
    and returns. The argument part is empty for now.
    
    The use of the `Entry` property on the calling conventions (in the
    .td files) is what make it possible to reuse them between FastISel
    and SDISel.

---
+++ llvm/lib/Target/H2BLB/H2BLBCallingConvention.cpp
@@ -0,0 +1,20 @@
+//=== H2BLBCallingConvention.cpp - H2BLB CC impl ----------------*- C++ -*-===//
+//
+// Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+//
+//===----------------------------------------------------------------------===//
+//
+// This file contains the table-generated and custom routines for the H2BLB
+// Calling Convention.
+//
+//===----------------------------------------------------------------------===//
+
+#include "H2BLBCallingConvention.h"
+#include "MCTargetDesc/H2BLBMCTargetDesc.h" // For H2BLB reg enums.
+using namespace llvm;
+
+// TableGen provides definitions of the calling convention analysis entry
+// points.
+#include "H2BLBGenCallingConv.inc"

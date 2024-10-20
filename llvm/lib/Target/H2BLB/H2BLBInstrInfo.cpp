e8829d692870fa4771d0d55fe9044eb38ea86680
Quentin Colombet <quentin.colombet@gmail.com>
2024-10-20 19:47:24 +0200

    [H2BLB][InstrInfo] Implement hooks for spills
    This commit implements the two hooks used to spill and reload values.
    Although this will be used by the register allocator when spilling value,
    they are also used by the target-independent code in the prologue/epilogue
    inserter to save and restore the callee saved registers.
    
    For the frame lowering, the CSRs can also be handled with the following
    two hooks:
    - TargetFrameLowering::spillCalleeSavedRegisters
    - TargetFrameLowering::restoreCalleeSavedRegisters
    
    For our target we do not use them because the regular spill/reload sequence
    is good enough.

--- llvm/lib/Target/H2BLB/H2BLBInstrInfo.cpp
+++ llvm/lib/Target/H2BLB/H2BLBInstrInfo.cpp
@@ -15,6 +15,7 @@
 #include "H2BLBRegisterInfo.h"
 #include "llvm/ADT/SmallVector.h"
 #include "llvm/CodeGen/MachineBasicBlock.h"
+#include "llvm/CodeGen/MachineFrameInfo.h"
 #include "llvm/CodeGen/MachineInstrBuilder.h"
 #include "llvm/IR/DebugLoc.h"
 #include "llvm/Support/ErrorHandling.h"
@@ -28,6 +29,50 @@ using namespace llvm;
 
 H2BLBInstrInfo::H2BLBInstrInfo()
     : H2BLBGenInstrInfo(H2BLB::ADJCALLSTACKDOWN, H2BLB::ADJCALLSTACKUP) {}
+
+void H2BLBInstrInfo::storeRegToStackSlot(MachineBasicBlock &MBB,
+                                         MachineBasicBlock::iterator MBBI,
+                                         Register SrcReg, bool isKill, int FI,
+                                         const TargetRegisterClass *RC,
+                                         const TargetRegisterInfo *TRI,
+                                         Register VReg) const {
+  MachineFunction &MF = *MBB.getParent();
+  MachineFrameInfo &MFI = MF.getFrameInfo();
+
+  MachinePointerInfo PtrInfo = MachinePointerInfo::getFixedStack(MF, FI);
+  MachineMemOperand *MMO =
+      MF.getMachineMemOperand(PtrInfo, MachineMemOperand::MOStore,
+                              MFI.getObjectSize(FI), MFI.getObjectAlign(FI));
+
+  unsigned Opc = TRI->getSpillSize(*RC) == 2 ? H2BLB::STRSP16 : H2BLB::STRSP32;
+  MFI.setStackID(FI, TargetStackID::Default);
+  BuildMI(MBB, MBBI, DebugLoc(), get(Opc))
+      .addReg(SrcReg, getKillRegState(isKill))
+      .addFrameIndex(FI)
+      .addImm(0)
+      .addMemOperand(MMO);
+}
+
+void H2BLBInstrInfo::loadRegFromStackSlot(MachineBasicBlock &MBB,
+                                          MachineBasicBlock::iterator MBBI,
+                                          Register DestReg, int FI,
+                                          const TargetRegisterClass *RC,
+                                          const TargetRegisterInfo *TRI,
+                                          Register VReg) const {
+  MachineFunction &MF = *MBB.getParent();
+  MachineFrameInfo &MFI = MF.getFrameInfo();
+  MachinePointerInfo PtrInfo = MachinePointerInfo::getFixedStack(MF, FI);
+  MachineMemOperand *MMO =
+      MF.getMachineMemOperand(PtrInfo, MachineMemOperand::MOLoad,
+                              MFI.getObjectSize(FI), MFI.getObjectAlign(FI));
+
+  unsigned Opc = TRI->getSpillSize(*RC) == 2 ? H2BLB::LDRSP16 : H2BLB::LDRSP32;
+  MFI.setStackID(FI, TargetStackID::Default);
+  BuildMI(MBB, MBBI, DebugLoc(), get(Opc), DestReg)
+      .addFrameIndex(FI)
+      .addImm(0)
+      .addMemOperand(MMO);
+}
 
 void H2BLBInstrInfo::copyPhysReg(MachineBasicBlock &MBB,
                                  MachineBasicBlock::iterator MI,

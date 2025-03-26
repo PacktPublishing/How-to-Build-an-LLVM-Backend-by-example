qcolombet/llvm-project

[H2BLB] Add a skeleton for the FrameLowering class
This class is referenced by the upcoming TableGen'ed RegisterInfo class

llvm/lib/Target/H2BLB/H2BLBFrameLowering.cpp
@@ -1,0 +1,31 @@
+//===-- H2BLBFrameLowering.cpp - H2BLB Frame Information ------------------===//
+//
+// Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+//
+//===----------------------------------------------------------------------===//
+//
+// This file contains the H2BLB implementation of TargetFrameLowering class.
+//
+//===----------------------------------------------------------------------===//
+
+#include "H2BLBFrameLowering.h"
+#include "H2BLBInstrInfo.h"
+#include "H2BLBSubtarget.h"
+#include "llvm/CodeGen/MachineFrameInfo.h"
+#include "llvm/CodeGen/MachineFunction.h"
+#include "llvm/CodeGen/MachineInstrBuilder.h"
+#include "llvm/CodeGen/MachineRegisterInfo.h"
+
+using namespace llvm;
+
+bool H2BLBFrameLowering::hasFP(const MachineFunction &MF) const {
+  return false;
+}
+
+void H2BLBFrameLowering::emitPrologue(MachineFunction &MF,
+                                      MachineBasicBlock &MBB) const {}
+
+void H2BLBFrameLowering::emitEpilogue(MachineFunction &MF,
+                                      MachineBasicBlock &MBB) const {}

llvm/lib/Target/H2BLB/H2BLBFrameLowering.h
@@ -1,0 +1,32 @@
+//===-- H2BLBFrameLowering.h - Define frame lowering -----------*- C++ -*--===//
+//
+// Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+//
+//===----------------------------------------------------------------------===//
+//
+// This class implements H2BLB-specific bits of TargetFrameLowering class.
+//
+//===----------------------------------------------------------------------===//
+
+#ifndef LLVM_LIB_TARGET_H2BLB_H2BLBFRAMELOWERING_H
+#define LLVM_LIB_TARGET_H2BLB_H2BLBFRAMELOWERING_H
+
+#include "llvm/CodeGen/TargetFrameLowering.h"
+
+namespace llvm {
+class H2BLBSubtarget;
+
+class H2BLBFrameLowering : public TargetFrameLowering {
+public:
+  explicit H2BLBFrameLowering(const H2BLBSubtarget &sti)
+      : TargetFrameLowering(TargetFrameLowering::StackGrowsDown, Align(8), 0) {}
+
+  void emitPrologue(MachineFunction &MF, MachineBasicBlock &MBB) const override;
+  void emitEpilogue(MachineFunction &MF, MachineBasicBlock &MBB) const override;
+
+  bool hasFP(const MachineFunction &MF) const override;
+};
+} // namespace llvm
+#endif

PacktPublishing/How-to-Build-an-LLVM-Backend-by-example

llvm/lib/Target/H2BLB/H2BLBFrameLowering.cpp
@@ -1,0 +1,31 @@
+//===-- H2BLBFrameLowering.cpp - H2BLB Frame Information ------------------===//
+//
+// Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+//
+//===----------------------------------------------------------------------===//
+//
+// This file contains the H2BLB implementation of TargetFrameLowering class.
+//
+//===----------------------------------------------------------------------===//
+
+#include "H2BLBFrameLowering.h"
+#include "H2BLBInstrInfo.h"
+#include "H2BLBSubtarget.h"
+#include "llvm/CodeGen/MachineFrameInfo.h"
+#include "llvm/CodeGen/MachineFunction.h"
+#include "llvm/CodeGen/MachineInstrBuilder.h"
+#include "llvm/CodeGen/MachineRegisterInfo.h"
+
+using namespace llvm;
+
+bool H2BLBFrameLowering::hasFPImpl(const MachineFunction &MF) const {
+  return false;
+}
+
+void H2BLBFrameLowering::emitPrologue(MachineFunction &MF,
+                                      MachineBasicBlock &MBB) const {}
+
+void H2BLBFrameLowering::emitEpilogue(MachineFunction &MF,
+                                      MachineBasicBlock &MBB) const {}

llvm/lib/Target/H2BLB/H2BLBFrameLowering.h
@@ -1,0 +1,33 @@
+//===-- H2BLBFrameLowering.h - Define frame lowering -----------*- C++ -*--===//
+//
+// Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+//
+//===----------------------------------------------------------------------===//
+//
+// This class implements H2BLB-specific bits of TargetFrameLowering class.
+//
+//===----------------------------------------------------------------------===//
+
+#ifndef LLVM_LIB_TARGET_H2BLB_H2BLBFRAMELOWERING_H
+#define LLVM_LIB_TARGET_H2BLB_H2BLBFRAMELOWERING_H
+
+#include "llvm/CodeGen/TargetFrameLowering.h"
+
+namespace llvm {
+class H2BLBSubtarget;
+
+class H2BLBFrameLowering : public TargetFrameLowering {
+protected:
+  bool hasFPImpl(const MachineFunction &MF) const override;
+
+public:
+  explicit H2BLBFrameLowering(const H2BLBSubtarget &sti)
+      : TargetFrameLowering(TargetFrameLowering::StackGrowsDown, Align(8), 0) {}
+
+  void emitPrologue(MachineFunction &MF, MachineBasicBlock &MBB) const override;
+  void emitEpilogue(MachineFunction &MF, MachineBasicBlock &MBB) const override;
+};
+} // namespace llvm
+#endif
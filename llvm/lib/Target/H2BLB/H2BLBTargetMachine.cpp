qcolombet/llvm-project

[H2BLB] Add a custom TargetPassConfig
This class is used to tweak the codegen pipeline for the specific
needs of a target.
This is just an empty shell for now.

llvm/lib/Target/H2BLB/H2BLBTargetMachine.cpp
@@ -15,7 +15,8 @@
 #include "H2BLBTargetObjectFile.h"
 #include "H2BLBTargetTransformInfo.h"
 #include "TargetInfo/H2BLBTargetInfo.h" // For getTheH2BLBTarget.
-#include "llvm/MC/TargetRegistry.h"     // For RegisterTargetMachine.
+#include "llvm/CodeGen/TargetPassConfig.h"
+#include "llvm/MC/TargetRegistry.h" // For RegisterTargetMachine.
 #include "llvm/Passes/PassBuilder.h"
 #include "llvm/Support/Compiler.h" // For LLVM_EXTERNAL_VISIBILITY.
 #include <memory>
@@ -96,3 +97,10 @@ void H2BLBTargetMachine::registerPassBuilderCallbacks(PassBuilder &PB) {
         MPM.addPass(createModuleToFunctionPassAdaptor(std::move(FPM)));
       });
 }
+
+TargetPassConfig *H2BLBTargetMachine::createPassConfig(PassManagerBase &PM) {
+  return new H2BLBPassConfig(*this, PM);
+}
+
+H2BLBPassConfig::H2BLBPassConfig(LLVMTargetMachine &TM, PassManagerBase &PM)
+    : TargetPassConfig(TM, PM) {}

llvm/lib/Target/H2BLB/H2BLBTargetMachine.h
@@ -14,6 +14,7 @@
 #define LLVM_LIB_TARGET_H2BLB_H2BLBTARGETMACHINE_H
 
 #include "H2BLBSubtarget.h"
+#include "llvm/CodeGen/TargetPassConfig.h"
 #include "llvm/Target/TargetLoweringObjectFile.h"
 #include "llvm/Target/TargetMachine.h"
 #include <memory>
@@ -42,6 +43,12 @@ public:
 
   // Register the target specific passes that this backend offers.
   void registerPassBuilderCallbacks(PassBuilder &PB) override;
+  TargetPassConfig *createPassConfig(PassManagerBase &PM) override;
+};
+
+class H2BLBPassConfig : public TargetPassConfig {
+public:
+  H2BLBPassConfig(LLVMTargetMachine &TM, PassManagerBase &PM);
 };
 
 } // end namespace llvm

PacktPublishing/How-to-Build-an-LLVM-Backend-by-example

llvm/lib/Target/H2BLB/H2BLBTargetMachine.cpp
@@ -15,7 +15,8 @@
 #include "H2BLBTargetObjectFile.h"
 #include "H2BLBTargetTransformInfo.h"
 #include "TargetInfo/H2BLBTargetInfo.h" // For getTheH2BLBTarget.
-#include "llvm/MC/TargetRegistry.h"     // For RegisterTargetMachine.
+#include "llvm/CodeGen/TargetPassConfig.h"
+#include "llvm/MC/TargetRegistry.h" // For RegisterTargetMachine.
 #include "llvm/Passes/PassBuilder.h"
 #include "llvm/Support/Compiler.h" // For LLVM_EXTERNAL_VISIBILITY.
 #include <memory>
@@ -96,3 +97,10 @@ void H2BLBTargetMachine::registerPassBuilderCallbacks(PassBuilder &PB) {
         MPM.addPass(createModuleToFunctionPassAdaptor(std::move(FPM)));
       });
 }
+
+TargetPassConfig *H2BLBTargetMachine::createPassConfig(PassManagerBase &PM) {
+  return new H2BLBPassConfig(*this, PM);
+}
+
+H2BLBPassConfig::H2BLBPassConfig(TargetMachine &TM, PassManagerBase &PM)
+    : TargetPassConfig(TM, PM) {}

llvm/lib/Target/H2BLB/H2BLBTargetMachine.h
@@ -14,6 +14,7 @@
 #define LLVM_LIB_TARGET_H2BLB_H2BLBTARGETMACHINE_H
 
 #include "H2BLBSubtarget.h"
+#include "llvm/CodeGen/TargetPassConfig.h"
 #include "llvm/Target/TargetLoweringObjectFile.h"
 #include "llvm/CodeGen/CodeGenTargetMachineImpl.h"
 #include <memory>
@@ -42,6 +43,12 @@ public:
 
   // Register the target specific passes that this backend offers.
   void registerPassBuilderCallbacks(PassBuilder &PB) override;
+  TargetPassConfig *createPassConfig(PassManagerBase &PM) override;
+};
+
+class H2BLBPassConfig : public TargetPassConfig {
+public:
+  H2BLBPassConfig(TargetMachine &TM, PassManagerBase &PM);
 };
 
 } // end namespace llvm